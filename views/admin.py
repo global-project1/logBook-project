3c486bc36d54aca3c5895b7c072e7316caf38906
Afanyuy Caleb <afanyuys@gmail.com>
2024-03-27 05:00:22 +0100

    working on the admin page

--- views/admin.py
+++ views/admin.py
@@ -1,31 +1,45 @@
 from tkinter import *
+import os
 import sys
 sys.path.append(r'C:\Users\Uzer\Desktop\python\logBook')
 import tkinter.messagebox as messageBox
-from . import shared_data
+import shared_data
 from models import students, classTab, courses, role, class_courses
+import ttkbootstrap as tb
 
 stud = students.Students()
 cour = courses.Courses()
 
 def adminHome():
 
-  def display_info():
-    my_list.delete(0, END)
-    for key in shared_data.studInfo:
-      my_list.insert(END,shared_data.studInfo[key])
+  def display_info(sub):
 
+    myFrame = Frame(root, bg="white", width="400", height="400")
+    myFrame.grid(row=4, column=0, columnspan=10)
 
-  def printIT(param):
-    print(param)
+    # Date entry
+    # cur_date = tb.DateEntry(myFrame, bootstyle="RoyalBlue1")
+    # cur_date.grid(row=5, column=2, columnspan=5)
+    
+    title = Label(myFrame, text = sub, font=('Acumin Variable Concept', 12, 'bold'), bg="white")
+    title.grid(row=1, column=2, columnspan=4)
+
+    btn = Button(myFrame, text="Add Record")
+    btn.grid(row=6, column=4)
+    
+
   # GUI 
       
   root = Tk()
-  root.geometry("600x600")
-
+  root.geometry("700x700")
   root.title("IAI logBook")
-
   root.resizable(False, False)
+
+  current_dir = os.getcwd()
+  
+  # icon
+  icon_img = current_dir + '/icon.ico'
+  root.iconbitmap(icon_img)
 
   # studName = shared_data.studInfo['name'] + "(Class Delegate)"
   homeLabel = Label(root, text="Class Delegate", font=('Acumin Variable Concept', 18, 'bold'))
@@ -39,17 +53,15 @@ def adminHome():
   if(status):
     options = [row[1] for row in courses]
   else:
-    messageBox.showerror("Error", "Erro loading options")
+    messageBox.showerror("Error", "Error loading options")
 
   sel_opt = StringVar()
   sel_opt.set("None")
-
-  option_menu = OptionMenu(root, sel_opt, *options, command=lambda opt=sel_opt: printIT(opt))
+  option_menu = OptionMenu(root, sel_opt, *options, command=lambda opt=sel_opt: display_info(opt))
   option_menu.place(relx=0.07, rely=0.2)
 
-  my_list = Listbox(root, height=15, width=60, bg="#fff")
-  my_list.place(relx=0.1, rely=0.3)
 
-  display_info()
 
   root.mainloop()
+
+adminHome()
